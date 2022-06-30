4b48d7d55fd7f6017d097b5fc8a30d56940f4755
Tanuj21Goswami <93069420+Tanuj21Goswami@users.noreply.github.com>
2022-06-30 14:44:32 +0530

    Add files via upload

---
+++ Sample1.cu
@@ -0,0 +1,24 @@
+/* Name - Tanuj Kumar
+   Entry No - 2020CSB1134 */
+int a=2+3*(14/7);
+int b;
+/*bnfrhnhfrne
+kjdfbkbfreukbrfk
+jdjnfnrl   */
+char *f;
+char *hello(int x);
+int main(int argc,char **argv)
+{
+    if(argc>2)
+    {
+        argc=0;
+    }
+    else 
+    {
+        return add(22,4);
+    }
+    while(2>=3)
+    {
+        func(2,3);
+    }
+}
---
+++ Sample2.cu
@@ -0,0 +1,22 @@
+/* Name - Tanuj Kumar
+   Entry No - 2020CSB1134 */
+char *f;
+int c=5-1;
+int func()
+{
+    /*bnfrhnhfrne
+    kjdfbkbfreukbrf
+    3rfmkmgtkm g;m  gggggggggggggggggggggggg           CS020k
+    jdjnfnrl   */
+    char **ptr;
+    while(a<b)
+    {
+        int y=0;
+    }
+    tent(2,b,c); /*func calls in function */
+    if(2>=1)
+    {
+        int x=1;
+    }
+    int s=3
+}
